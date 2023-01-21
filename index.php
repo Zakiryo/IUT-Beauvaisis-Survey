975ecf82eed9fda8c1c1a1336b09bdf5717c2d47
Zakiryo <yohan.rudny@outlook.fr>
2023-01-21 14:45:22 +0100

    Début de la page d'accueil

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Sondage alimentaire - Beauvaisis</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="img/logo.png">
+</head>
+
+<body style="width: 1500px">
+    <div class="header">
+        <img src="img/logo.png" width="235" height="120" alt="">
+        <h1>Sondage alimentaire du Beauvaisis</h1>
+        <button class="button-28" role="button">Plus d'informations sur le sondage</button>
+    </div>
+    <hr>
+    <div class="intro">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div class="buttonSurvey">
+        <button class="button-28" role="button" style="background-color: rgba(17, 42, 126, 0.452);">Répondre au sondage</button>
+        <button class="button-28" role="button" style="background-color: rgba(139, 6, 6, 0.452);">Consulter les résultats</button>
+    </div>
+</body>
+
+</html>
